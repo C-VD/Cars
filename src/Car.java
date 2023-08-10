24d10c55742ec47774b8830df72f94d7e13dc202
C-VD <vldr@disroot.org>
2023-08-10 15:40:08 +0300

    TreeSet

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,6 @@
-public class Car {
+import java.util.Objects;
+
+public class Car implements Comparable {
     String name;
     int gruz;
     int speed;
@@ -7,6 +9,21 @@ public class Car {
         this.name = name;
         this.gruz = gruz;
         this.speed = speed;
+    }
+    public int compareTo(Object o){
+        if (o instanceof Car) {
+            Car c = (Car) o;
+            return Integer.compare(this.gruz, c.gruz);
+        }
+        return 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return gruz == car.gruz && speed == car.speed && Objects.equals(name, car.name);
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
         // Получаем данные о машинках
-        ArrayList<Car> cars = new ArrayList<>();
+        TreeSet<Car> cars = new TreeSet<>();
         cars.add(new Car("ВАЗик", 1000, 80));
         cars.add(new Car("ГАЗик", 1000, 70));
         cars.add(new Car("ПАЗик", 1000, 85));
-        cars.add(new Car("УАЗик", 1000, 750));
+        cars.add(new Car("УАЗик", 1000, 75));
+        cars.add(new Car("ВАЗ", 1500, 80));
+        cars.add(new Car("ГАЗ", 1700, 70));
+        cars.add(new Car("ПАЗ", 2000, 85));
+        cars.add(new Car("УАЗ", 1600, 75));
+        cars.add(new Car("БАЗ", 1600, 75));
+        cars.add(new Car("Ока", 2100, 200));
 
         // СКОРОСТЬ
         // Сортируем
-        cars.sort((a, b) -> Integer.compare(a.speed, b.speed));
+        //cars.sort((a, b) -> Integer.compare(a.speed, b.speed));
         // Выводим
         System.out.println(cars.toString());
 
         // ИМЯ
         // Сортируем
-        cars.sort((a, b) -> CharSequence.compare(a.name, b.name));
+        //cars.sort((a, b) -> CharSequence.compare(a.name, b.name));
         // Выводим
         System.out.println(cars.toString());
     }
